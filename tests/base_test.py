a5a48372f3664d2177b16978d30d15328d7937b8
Danila Shtan <danila@shtan.ru>
2016-12-16 16:44:30 +0500

    test fix

--- tests/base_test.py
+++ tests/base_test.py
@@ -28,7 +28,7 @@ class BaseTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super(BaseTest, cls).setUpClass()
-        cls.watcher = ServiceWatcher()
+        cls.watcher = ServiceWatcher().watch_services()
         threading.Thread(target=start_tornado).start()
 
     @classmethod
